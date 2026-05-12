77552e33b4a777a14c55ae234e6675fd5755f361
Dean <traviezomazariegos@hotmail.com>
2026-05-12 11:31:40 -0600

    Relacion ManyToOne entre Admin y Product

--- src/main/java/com/projectinventory/veggtables/entity/Admin.java
+++ src/main/java/com/projectinventory/veggtables/entity/Admin.java
@@ -1,13 +1,17 @@
 package com.projectinventory.veggtables.entity;
 
 import java.io.Serializable;
+import java.util.List;
 
 import jakarta.persistence.Basic;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -35,6 +39,9 @@ public class Admin implements Serializable {
 	
 	@Column(name="status")
 	private Boolean status;
+	
+	@OneToMany(mappedBy = "admin", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+	private List<Product> productos;
 	
 	public Integer getIdadmin() {
 		return idadmin;
--- src/main/java/com/projectinventory/veggtables/entity/product.java
+++ src/main/java/com/projectinventory/veggtables/entity/product.java
@@ -2,8 +2,6 @@ package com.projectinventory.veggtables.entity;
 
 import java.io.Serializable;
 
-
-
 import jakarta.persistence.Basic;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -17,11 +15,10 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name="product")
-public class product implements Serializable{
+public class Product implements Serializable{
 
 	
-	private static final long serialVersionUID = 553060036124245313L;
-	
+	private static final long serialVersionUID = -7468567075377356556L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,52 +42,6 @@ public class product implements Serializable{
 	@JoinColumn(name="admin_idadmin")
 	private Admin admin;
 
-	public Integer getIdproduct() {
-		return idproduct;
-	}
-
-	public void setIdproduct(Integer idproduct) {
-		this.idproduct = idproduct;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-	public Integer getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
-
-	public String getCategory() {
-		return category;
-	}
-
-	public void setCategory(String category) {
-		this.category = category;
-	}
-
-	public Admin getAdmin() {
-		return admin;
-	}
-
-	public void setAdmin(Admin admin) {
-		this.admin = admin;
-	}
 	
+
 }
