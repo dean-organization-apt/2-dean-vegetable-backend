5adff5eba1f9349a64a04b64c984d949af205027
Dean <traviezomazariegos@hotmail.com>
2026-05-12 11:26:32 -0600

    added the entity for product and fk onetomany

---
+++ src/main/java/com/projectinventory/veggtables/entity/product.java
@@ -0,0 +1,96 @@
+package com.projectinventory.veggtables.entity;
+
+import java.io.Serializable;
+
+
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="product")
+public class product implements Serializable{
+
+	
+	private static final long serialVersionUID = 553060036124245313L;
+	
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name="idproduct")
+	private Integer idproduct;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="price")
+	private Double price; 
+	
+	@Column(name="quantity")
+	private Integer quantity;
+	
+	@Column(name="category")
+	private String category;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="admin_idadmin")
+	private Admin admin;
+
+	public Integer getIdproduct() {
+		return idproduct;
+	}
+
+	public void setIdproduct(Integer idproduct) {
+		this.idproduct = idproduct;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public Admin getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(Admin admin) {
+		this.admin = admin;
+	}
+	
+}
